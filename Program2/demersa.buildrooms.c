ecf670907b46a80be07b3834d97e4df6fd287f07
Andrew Demers <andrew.demers91@gmail.com>
2018-10-19 21:01:24 -0500

    99% complete.

--- Program2/demersa.buildrooms.c
+++ Program2/demersa.buildrooms.c
@@ -26,7 +26,7 @@ struct Room {
     int numConnections;
     char* roomName;
     enum RoomType roomType;
-    struct room** outBoundConnections[6];    // stores pointer to room connections
+    struct Room* outBoundConnections[6];    // stores pointer to room connections
     FILE* currentDir;
 };
 
@@ -54,7 +54,7 @@ char* genRooms() {
     int i =0;
 
     // loops through array 7 times and pick a random name each time
-    for (i = 0; i < 7; i++) {
+    for (i = 0; i < MAX_NUM_ROOMS; i++) {
         int index = rand() % (namesCount - i);  // random index between 0 and last element in array
         roomList[i].roomName = names[index];    // store name
         names[index] = names[namesCount - i - 1];   // overwrite with the last element in array
@@ -63,25 +63,34 @@ char* genRooms() {
     }
 }
 
-// creates seven different room files
-FILE** createFiles(char* dirName) {
-    // allocate memory
-    FILE** filePtr = malloc(7 * sizeof(FILE**));
+// creates seven different room files and fills with info
+void createFiles(char* dirName) {
+    FILE* filePath[256];
+    FILE* filePtr;
 
-    char filePath[256];
+    // create file for each room name and files
+    for (int i =0; i < MAX_NUM_ROOMS; i++) {
+        sprintf(filePath, "%s/%s.txt", dirName, roomList[i].roomName);  // create file name
+        filePtr = fopen(filePath, "w");      // write file
+        fprintf(filePtr, "ROOM NAME: %s\n", roomList[i].roomName);     // writes room name to file
+        for (int j = 0; j < roomList[i].numConnections; j++) {      // iterates through each room connection and writes connection to file
+            fprintf(filePtr, "CONNECTION %d: %s\n", j+1, roomList[i].outBoundConnections[j]->roomName);
+        }
 
-    // create file for each room name
-    for (int i =0; i < 7; i++) {
-        sprintf(filePath, "%s/%s.txt", dirName, roomList[i].roomName);
-        filePtr[i] = fopen(filePath, "a+");
-
-        //printf("%s\n", filePath);   // TODO: testing only - DELETE
+        if (roomList[i].roomType == 0) {        // prints room type since enum strings cannot be printed
+            fprintf(filePtr, "ROOM TYPE: START_ROOM\n");
+        }
+        else if (roomList[i].roomType == 1) {
+            fprintf(filePtr, "ROOM TYPE: MID_ROOM\n");
+        }
+        else if (roomList[i].roomType == 2) {
+            fprintf(filePtr, "ROOM TYPE: END_ROOM\n");
+        }
     }
-  return filePtr;
 }
 
 // initializes room structs
-struct Room* initRooms() {
+void initRooms() {
 
     // set names to names picked above
     for (int i = 0; i < MAX_NUM_ROOMS; i++) {
@@ -124,20 +133,14 @@ bool isGraphFull() {
 }
 
 // returns a random Room, does not validate if connection can be added
-struct Room getRandomRoom() {
+int getRandomRoom() {
     int randomNum = rand() % 7;     // returns random number between 0 and 6
 
-    //printf(roomList[randomNum].roomName);  // TODO: testing - DELETE
-    //printf("\n");
-
-    return roomList[randomNum];
+    return randomNum;
 }
 
-
-/* ****************EVERYTHING ABOVE HERE WORKS******************* */
-
 // returns true if a connection can be added from Room (< 6 outbound connections), false otherwise
-int canAddConnectionFrom(struct Room room) {
+bool canAddConnectionFrom(struct Room room) {
     if (room.numConnections < 6) {
         return TRUE;
     }
@@ -145,11 +148,11 @@ int canAddConnectionFrom(struct Room room) {
 }
 
 // returns true if a connection from Room 1 to Room 2 already exists, false otherwise
-int connectionAlreadyExists(struct Room room1, struct Room room2) {
+bool connectionAlreadyExists(int room1Pos, int Room2Pos) {
     int i = 0;
 
-    while(room1.outBoundConnections[i] != NULL) {      // while there are still connections
-        if(strcmp(room1.outBoundConnections[i], room2.roomName) == 0)     // if Room 1 connection matches Room 2
+    while(roomList[room1Pos].outBoundConnections[i] != NULL) {      // while there are still connections
+        if(strcmp(roomList[room1Pos].outBoundConnections[i], roomList[Room2Pos].roomName) == 0)     // if Room 1 connection matches Room 2
             return TRUE;       // return true
         else
             i++;
@@ -158,72 +161,73 @@ int connectionAlreadyExists(struct Room room1, struct Room room2) {
 }
 
 // connects Rooms 1 and 2 together, does not check if this connection is valid
-void connectRoom(struct Room room1, struct Room room2) {
+void connectRoom(int room1Pos, int room2Pos) {
     int i = 0;
 
-    if (connectionAlreadyExists(room1, room2) == FALSE) {   // if there is not a connection between the two rooms
-        while (room1.outBoundConnections[i] != NULL){      // iterate through the list of connections and find the first empty spot
+
+    if (connectionAlreadyExists(room1Pos, room2Pos) == FALSE) {   // if there is not a connection between the two rooms
+        while (roomList[room1Pos].outBoundConnections[i] != NULL){      // iterate through the list of connections and find the first empty spot
             i++;
         }
-        room1.outBoundConnections[i] = room2.roomName;
+        roomList[room1Pos].outBoundConnections[i] = &roomList[room2Pos];
+        roomList[room2Pos].outBoundConnections[i] = &roomList[room1Pos];
+
+        // increment number of connections for both rooms
+        roomList[room1Pos].numConnections++;
+        roomList[room2Pos].numConnections++;
     }
 }
 
 // returns true if Rooms 1 and 2 are the same Room, false otherwise
-int isSameRoom(struct Room room1, struct Room room2) {
-    if (strcmp(room1.roomName, room2.roomName) == 0)
+bool isSameRoom(int room1Pos, int room2Pos) {
+    if (strcmp(roomList[room1Pos].roomName, roomList[room2Pos].roomName) == 0)
         return TRUE;
     return FALSE;
 }
-/*
+
 // adds a random, valid outbound connection from one Room to another Room
-void addRandomConnection(struct Room** gameRooms) {
-    struct Room** room1;
-    struct Room** room2;
+void addRandomConnection() {
+    int room1Pos = 0;
+    int room2Pos = 0;
 
     while(TRUE) {
-        room1 = getRandomRoom(gameRooms);
+        room1Pos = getRandomRoom();
 
-        if (canAddConnectionFrom(room1) == TRUE)
+        if (canAddConnectionFrom(roomList[room1Pos]) == TRUE)
             break;
     }
 
     do {
-        room2 = getRandomRoom(gameRooms);
+        room2Pos = getRandomRoom();
     }
-    while(canAddConnectionFrom(room2) == FALSE || isSameRoom(room1, room2) == TRUE || connectionAlreadyExists(room1, room2) == TRUE); {
+    while(canAddConnectionFrom(roomList[room2Pos]) == FALSE || isSameRoom(room1Pos, room2Pos) == TRUE || connectionAlreadyExists(room1Pos, room2Pos) == TRUE);
 
-        connectRoom(room1, room2);
-        connectRoom(room2, room1);
-    }
+    connectRoom(room1Pos, room2Pos);
+    connectRoom(room2Pos, room1Pos);
 }
+
 
 // iterate through whole list of rooms and create connections for each one
-void initGameRooms (struct Room** gameRooms) {
-    while (isGraphFull(gameRooms) == FALSE) {
-        addRandomConnection(gameRooms);
+void initGameRooms () {
+    while (isGraphFull() == FALSE) {
+        addRandomConnection();
     }
 }
-*/
+
 
 // write the room list created in initGameRooms to the files created in createFiles
+void writeRoomList() {
+    char* dirName;
+    FILE *filePtr;
 
-
+    dirName = createDir();      // creates directory
+    createFiles(dirName);  // creates the files for the directory
+}
 
 int main() {
     srand(time(NULL));      // initialize random seed
-    char* dirName = createDir();
     genRooms();
-    FILE** filePtr = createFiles(dirName);
     initRooms();
-    isGraphFull();
-    getRandomRoom();
-
-
-    //struct Room** gameRooms = initRooms(roomNames);
-    //getRandomRoom(gameRooms);       // TODO: testing only - DELETE
-    //addRandomConnection(gameRooms);     // TODO: testing only - DELETE
-    //initGameRooms(gameRooms);
+    initGameRooms();
+    writeRoomList();
 }
-
-// need to fix outbound connections; probably addRandomConnections function
